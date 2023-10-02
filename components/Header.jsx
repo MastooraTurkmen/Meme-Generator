8f04c737e029985b11bf04ac33bd1c9729428437
MastooraTurkmen <132576850+MastooraTurkmen@users.noreply.github.com>
2023-10-02 15:46:29 +0430

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import trollFace from "../images/troll-face.png";
 
 export default function Header() {
   return (
     <header className="header">
-      <img src={trollFace} className="header--image" />
+      <img src="https://i.postimg.cc/nrZtX5sg/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
